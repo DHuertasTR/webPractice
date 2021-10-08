a6a6e85a12d58c965ebaf91e2d7ab454c2eaad29
david huertas <aletru321@gmail.com>
2021-10-08 01:39:51 -0500

    prettier and eslint

--- StoryMaker/src/App.js
+++ StoryMaker/src/App.js
@@ -1,25 +1,23 @@
-
-
 const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h2", {}, props.namae),
-        React.createElement("h3", {}, props.raze),
-        React.createElement("h3", {}, props.tipe),
-        
-
-    ])
-
-}
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.namae),
+    React.createElement("h3", {}, props.raze),
+    React.createElement("h3", {}, props.tipe),
+  ]);
+};
 
 function App() {
-    return React.createElement(
-        "div",
-        {},
-        React.createElement("h1", {id: 'dogo'}, 'write a history'),
-        React.createElement(Pet,{namae: 'sakamoto', raze: 'dogo', tipe: 'labrator'}),
-        React.createElement(Pet,{namae: 'sakura', raze: 'cat', tipe: 'orange'}),
-        React.createElement(Pet,{namae: 'dickhead', raze: 'fish', tipe: 'pargo'}),
-
-    );
-} 
-ReactDOM.render(React.createElement(App), document.getElementById('root'))
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", { id: "dogo" }, "write a history"),
+    React.createElement(Pet, {
+      namae: "sakamoto",
+      raze: "dogo",
+      tipe: "labrator",
+    }),
+    React.createElement(Pet, { namae: "sakura", raze: "cat", tipe: "orange" }),
+    React.createElement(Pet, { namae: "dickhead", raze: "fish", tipe: "pargo" })
+  );
+}
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
